a8c2e4b728c7b6ef051d0eb99fb1207573e67bba
bhavin gandha <bhavin.g@yudiz.in>
2018-06-27 12:13:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const vision = require('@google-cloud/vision');
+const client = new vision.ImageAnnotatorClient();
+
+client
+  .documentTextDetection('./public/c.jpeg')
+  .then(results => {
+    console.log(JSON.stringify(results[0].textAnnotations, null, 2))
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+
+
+
+  // documentTextDetection
+  // faceDetection -> results[0].faceAnnotations
+  // labelDetection -> results[0].labelAnnotations
+  // landmarkDetection -> results[0].landmarkAnnotations
+  // textDetection -> results[0].textAnnotations
+  // logoDetection -> results[0].logoAnnotations
+  // imageProperties -> results[0].imagePropertiesAnnotation
+  // safeSearchDetection -> results[0].safeSearchAnnotation
+  // cropHints -> results[0].cropHintsAnnotation
+  // webDetection -> results[0].webDetection 
